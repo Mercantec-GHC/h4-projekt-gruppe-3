6cae367fc0c54605169280f6b18a745de085f4c8
krc-coding <kasper@ulvedalen.dk>
2024-08-14 12:52:06 +0200

    Initial passport setup

---
+++ API/app/Http/Controllers/UserAuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
+
+class UserAuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'username' => 'required|unique:users,username',
+            'is_parent' => 'required|boolean',
+            'email' => 'sometimes|required|email:rfc,dns|unique:users,email',
+            'password' => ['required', 'confirmed', Password::min(8)->letters()->mixedCase()->numbers()],
+        ]);
+
+        $data['password'] = Hash::make($request->password);
+
+        $photo = $request->photo;
+        $data['profile_picture'] = 'photo not implemented yet';
+
+        $user = User::create(Arr::except($data, 'photo'));
+
+        $token = $user->createToken('API Token')->accessToken;
+
+        return response()->json(['user' => $user, 'token' => $token]);
+    }
+
+    public function login(Request $request)
+    {
+        $data = $request->validate([
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        if (!auth()->attempt($data)) {
+            return response(['error_message' => 'Incorrect Details. 
+            Please try again']);
+        }
+
+        $token = auth()->user()->createToken('API Token')->accessToken;
+
+        return response(['user' => auth()->user(), 'token' => $token]);
+    }
+}
