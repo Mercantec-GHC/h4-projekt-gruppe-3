fc70847bf3cf39d630a07e3c486e922162f65757
BobCabossa <148852994+BobCabossa@users.noreply.github.com>
2024-09-04 11:46:05 +0200

    Update Mobile/lib/Components/ColorScheme.dart
    Co-authored-by: Kasper <kasper@ulvedalen.dk>

--- Mobile/lib/Components/ColorScheme.dart
+++ Mobile/lib/Components/ColorScheme.dart
@@ -7,8 +7,8 @@ class CustomColorScheme {
   static Color primary = const Color.fromRGBO(194, 232, 255, 1);
   static Color secondary = const Color.fromRGBO(245, 197, 58, 1);
   static Color error = const Color.fromRGBO(238, 100, 118, 1);
-  static Color randomColor = const Color.fromRGBO(137, 213, 107, 1);
-  static Color randomColorTwo = const Color.fromRGBO(240, 110, 81, 1);
+  static Color limeGreen = const Color.fromRGBO(137, 213, 107, 1);
+  static Color tangerineOrange = const Color.fromRGBO(240, 110, 81, 1);
   static Color menu = const Color.fromRGBO(217, 217, 217, 1);
 
   static List<Color> taskCardColorSheme = [
