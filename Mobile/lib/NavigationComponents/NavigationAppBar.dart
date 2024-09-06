ecc8599ff44f77fbb51c91ed4da3c419a2f8c3f2
krc-coding <kasper@ulvedalen.dk>
2024-09-06 10:57:27 +0200

    Created navigation app bar component

---
+++ Mobile/lib/NavigationComponents/NavigationAppBar.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/Components/ColorScheme.dart';
+import 'package:mobile/config/general_config.dart';
+import 'package:mobile/services/app_state.dart';
+
+class NavAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final RootAppState appState;
+  final String? page_title;
+  final String? jwt;
+
+  const NavAppBar({
+    super.key,
+    required this.appState,
+    required this.page_title,
+    required this.jwt,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: CustomColorScheme.secondary,
+      title: appState.user?.isParent ?? false
+          ? Text(page_title ?? '')
+          : Card(
+              color: Colors.green,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(appState.points.toString() + 'p'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+      centerTitle: true,
+      actions: [
+        Container(
+          width: 75,
+          child: Center(
+            child: CircleAvatar(
+              radius: 15,
+              backgroundImage: appState.user != null
+                  ? NetworkImage(
+                      baseUrl +
+                          '/api/user/${appState.user?.id}/profile/picture',
+                    )
+                  : const AssetImage('assets/default_profile.png')
+                      as ImageProvider, // Default image if no URL
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(50);
+}
