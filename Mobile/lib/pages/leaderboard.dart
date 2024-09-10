8fb561869277778f5fb15479fef2490794a6a026
Kasper K <Kasperklassen@gmail.com>
2024-09-10 09:23:05 +0200

    Update leaderboard.dart

--- Mobile/lib/pages/leaderboard.dart
+++ Mobile/lib/pages/leaderboard.dart
@@ -48,77 +48,61 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
           Positioned.fill(
             child: MeshGradientBackground(),
           ),
-          SizedBox(
-            height: 600,
-            child: Card(
-              color: CustomColorScheme.menu,
-              elevation: 4,
-              margin: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: CustomColorScheme.secondary,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(10.0),
-                        topRight: Radius.circular(10.0),
+          Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.transparent,
+                ),
+                padding: EdgeInsets.all(16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Text(
+                        'Leaderboard',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
-                    padding: EdgeInsets.all(16),
-                    child: Row(
+                    if (_appState.user?.isParent ?? false)
+                      IconButton(
+                        icon: Icon(Icons.add),
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) =>
+                                Childcreation(onCreation: childCreation),
+                          );
+                        },
+                      ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                  ),
+                  child: SingleChildScrollView(
+                    child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Expanded(
-                          child: Text(
-                            'Leaderboard',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                        if (_appState.user?.isParent ?? false)
-                          IconButton(
-                            icon: Icon(Icons.add),
-                            onPressed: () {
-                              showDialog(
-                                context: context,
-                                builder: (context) =>
-                                    Childcreation(onCreation: childCreation),
-                              );
-                            },
+                        for (UserProfile userProfile in users)
+                          Userprofilecard(
+                            name: userProfile.name,
+                            points: userProfile.total_points,
+                            page: 'leaderboard',
                           ),
                       ],
                     ),
                   ),
-                  Expanded(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: CustomColorScheme.menu,
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(10.0),
-                          bottomRight: Radius.circular(10.0),
-                        ),
-                      ),
-                      child: SingleChildScrollView(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            for (UserProfile userProfile in users)
-                              Userprofilecard(
-                                name: userProfile.name,
-                                points: userProfile.total_points,
-                                page: 'leaderboard',
-                              ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
+            ],
           ),
         ],
       ),
