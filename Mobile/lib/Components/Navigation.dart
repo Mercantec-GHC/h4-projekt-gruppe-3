b186004c0824f3e0bdb4048863b16e76c9e7caf4
JonasNJensen <102585691+JonasNJensen@users.noreply.github.com>
2024-09-12 11:03:39 +0200

    Update Navigation.dart

--- Mobile/lib/Components/Navigation.dart
+++ Mobile/lib/Components/Navigation.dart
@@ -164,7 +164,7 @@ class _NavigationComponentState extends State<NavigationComponent> {
             onTap: () {}, // Prevents the panel from closing when tapped inside
             child: Container(
               width: 200,
-              height: 130,
+              height: _appState.family!.name.length < 11 ? 160 : 190,
               decoration: BoxDecoration(
                 color: CustomColorScheme.menu,
                 borderRadius: BorderRadius.circular(10),
@@ -183,6 +183,12 @@ class _NavigationComponentState extends State<NavigationComponent> {
                     Center(
                         child: Text(
                       "Profile menu",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    )),
+                    Center(
+                        child: Text(
+                      "family: " + _appState.family!.name,
                       style:
                           TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                     )),
