3b16a4e503b87753d75b01959b0376548968e939
krc-coding <kasper@ulvedalen.dk>
2024-09-12 10:53:51 +0200

    Updated task approval dialog to use user select dropdown and show completion photo

--- Mobile/lib/Components/TaskApprovalDialog.dart
+++ Mobile/lib/Components/TaskApprovalDialog.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'package:mobile/Components/ColorScheme.dart';
 import 'package:mobile/Components/CustomPopup.dart';
 import 'package:mobile/config/general_config.dart';
 import 'package:mobile/models/task.dart';
@@ -24,46 +24,68 @@ class TaskApprovalDialog extends StatefulWidget {
 }
 
 class _TaskApprovalDialogState extends State<TaskApprovalDialog> {
-  late RootAppState _appState;
-  String? _auth_token;
-  bool _loaded = false;
   final Api _api = Api();
+  late RootAppState _appState;
+  late List<User> _assignedUsers;
+  late User _selectedUser;
+  late String _auth_token;
+  bool _loaded = false;
 
   void _approveTask() async {
-    // if (result.statusCode == 204) {
-    //   Navigator.of(context).pop();
-    //   return;
-    // }
+    Response response = await _api.approveTaskForUser(
+      auth_token: _auth_token,
+      task_id: widget.task.id,
+      user_id: _selectedUser.id,
+    );
+
+    if (response.statusCode == 204) {
+      Navigator.of(context).pop();
+      return;
+    }
   }
 
-  void _declineTask() async {
-    // if (result.statusCode == 204) {
-    //   Navigator.of(context).pop();
-    //   return;
-    // }
-  }
+  void _getPendingTaskUsers() async {
+    final jwt = await _appState.storage.read(key: 'auth_token');
+    if (jwt == null) {
+      return;
+    }
 
-  // Future<List<User>> _getuserAssignedToThisTask() async {
-  //   Response response =
-  //       await _api.getUsersAssignToTask(widget.task.id, _appState);
-  //   var jsonData = json.decode(response.body);
-  //   if (response.statusCode == 200) {
-  //     List<User> newUsers = [];
-  //     for (var user in jsonData) {
-  //       newUsers.add(new User(user['id'], user['name'], user['email'],
-  //           _getBool(user['is_parent'])));
-  //     }
-  //   } else {
-  //     CustomPopup.openErrorPopup(context, errorText: jsonData['Error']);
-  //     return [];
-  //   }
-  // }
+    Response response = await _api.getPendingTaskUsers(
+      auth_token: jwt,
+      task_id: widget.task.id,
+    );
+    var jsonData = json.decode(response.body);
+    List<User> users = [];
+    if (response.statusCode == 200) {
+      for (var user in jsonData) {
+        users.add(
+          new User(
+            user['id'],
+            user['name'],
+            '', // The email isn't required in this case as, this is only used to get name and id of the user
+          ),
+        );
+      }
+    } else {
+      CustomPopup.openErrorPopup(context, errorText: jsonData);
+      return;
+    }
 
-  void _getAuthToken() async {
-    String? _token = await _appState.storage.read(key: 'auth_token');
     setState(() {
-      _auth_token = _token;
+      _assignedUsers = users;
+      _selectedUser = users.first;
+      _auth_token = jwt;
       _loaded = true;
+    });
+  }
+
+  void _changeSelectedUser(User? user) {
+    if (user == null) {
+      return;
+    }
+
+    setState(() {
+      _selectedUser = user;
     });
   }
 
@@ -71,14 +93,14 @@ class _TaskApprovalDialogState extends State<TaskApprovalDialog> {
   void initState() {
     super.initState();
     _appState = Provider.of<RootAppState>(context, listen: false);
-    _getAuthToken();
+    _getPendingTaskUsers();
   }
 
   @override
   Widget build(BuildContext context) {
     _appState = context.watch<RootAppState>();
     return AlertDialog(
-      backgroundColor: Color(0xFF89D56B),
+      backgroundColor: CustomColorScheme.primary,
       title: Text(widget.task.title),
       actions: [
         TextButton(
@@ -91,20 +113,29 @@ class _TaskApprovalDialogState extends State<TaskApprovalDialog> {
           onPressed: _approveTask,
           child: Text('Approve'),
         ),
-        TextButton(
-          onPressed: _declineTask,
-          child: Text('Decline'),
-        ),
       ],
       content: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
+          Text('Select child:'),
+          if (_loaded)
+            DropdownButton(
+              value: _selectedUser,
+              items: _assignedUsers
+                  .map((user) => DropdownMenuItem(
+                        value: user,
+                        child: Text(user.name),
+                      ))
+                  .toList(),
+              onChanged: _changeSelectedUser,
+            ),
           SizedBox(
             height: 15,
           ),
           if (_loaded)
             Image.network(
-              baseUrl + '/api/task/${widget.task.id}/completion-photo',
+              baseUrl +
+                  '/api/task/${widget.task.id}/user/${_selectedUser.id}/completion-photo',
               headers: {
                 HttpHeaders.authorizationHeader: 'Bearer ${_auth_token}'
               },
